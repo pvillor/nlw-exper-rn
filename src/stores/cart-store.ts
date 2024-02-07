4222272fe36fe5535fe9a06b46afe1499cd0ad22
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-02-07 17:51:42 -0400

    feat: remove products

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,10 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 import * as cartInMemory from "./helpers/cart-in-memory";
+import AsyncStorage, {
+  AsyncStorageStatic,
+} from "@react-native-async-storage/async-storage";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export type ProductCartProps = ProductProps & {
   quantity: number;
@@ -9,12 +13,25 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[];
   add: (product: ProductProps) => void;
+  remove: (productId: string) => void;
 };
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
-}));
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
+      remove: (productId: string) =>
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        })),
+    }),
+    {
+      name: "nlw-expert:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
